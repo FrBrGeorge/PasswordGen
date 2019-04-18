01b565fae2b74fded0db6c19e9b0da0c5bc6ca99
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-04-18 15:31:56 +0300

    Fix net architecture

---
+++ PasswordGen/net.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+Get a word dictionary from various sources
+
+Now supports only https://github.com/first20hours/google-10000-english
+'''
+# python3 -c 'import urllib.request; import random; print(" ".join(random.sample(urllib.request.urlopen("https://github.com/first20hours/google-10000-english/raw/master/google-10000-english.txt").read().decode().split(),4)))'
+#
+# TODO linux words http://www.ibiblio.org/pub/Linux/libs/linux.words.2.tar.gz
+
+from urllib.request import urlopen
+from collections import namedtuple
+
+def get_txt(name):
+    '''Get textfile with words
+    E. g. from https://github.com/first20hours/google-10000-english
+    '''
+    return lib.request.urlopen(URLS[name].URL).read().decode().split()
+
+Site = namedtuple("URLS", "URL get")
+
+URLS = { "google-10000-english": Site("https://github.com/first20hours/google-10000-english/raw/master/google-10000-english.txt", get_txt) }
