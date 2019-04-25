1c12375a1dafd1c02d6b37dbb6e0132d64f99c94
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2019-04-25 10:35:07 +0300

    Link widgets to App.self

--- pg.py
+++ pg.py
@@ -27,33 +27,34 @@ class App(Frame):
         self.master.title(title)
         self.grid(sticky="NEWS")
 
+        self.vAlg = StringVar(self, "Select algorithm")
         fAlg = MockWidget(self, text="Algorithm", context="Algorithm menubutton")
         fAlg.grid(row=0, column=0, sticky="NEWS")
         self.rowconfigure(0, weight=0)
 
-        fPar = MockWidget(self, text="Algorithm parameters", context="Algorithm parameters")
-        fPar.grid(row=1, column=0, sticky="NEWS")
+        self.fPar = MockWidget(self, text="Algorithm parameters", context="Algorithm parameters")
+        self.fPar.grid(row=1, column=0, sticky="NEWS")
         self.rowconfigure(1, weight=0)
 
-        fVoc = MockWidget(self, text="Dictionary", context="Dictionary label and menubutton")
-        fVoc.grid(row=2, column=0, sticky="NEWS")
+        self.fVoc = MockWidget(self, text="Dictionary", context="Dictionary label and menubutton")
+        self.fVoc.grid(row=2, column=0, sticky="NEWS")
         self.rowconfigure(2, weight=0)
 
-        fSep = MockWidget(self, text="Separators", context="Separators label and menubutton")
-        fSep.grid(row=3, column=0, sticky="NEWS")
+        self.fSep = MockWidget(self, text="Separators", context="Separators label and menubutton")
+        self.fSep.grid(row=3, column=0, sticky="NEWS")
         self.rowconfigure(3, weight=0)
 
-        fPass = MockWidget(self, text="Passwords", context="Passwords multiline label and")
-        fPass.grid(row=4, column=0, sticky="NEWS")
+        self.fPass = MockWidget(self, text="Passwords", context="Passwords multiline label and")
+        self.fPass.grid(row=4, column=0, sticky="NEWS")
         self.rowconfigure(4, weight=1)
 
-        fGen = MockWidget(self, text="Generate", context="Passphrase generation controls")
-        fGen.grid(row=5, column=0, sticky="NEWS")
+        self.fGen = MockWidget(self, text="Generate", context="Passphrase generation controls")
+        self.fGen.grid(row=5, column=0, sticky="NEWS")
         self.rowconfigure(5, weight=0)
 
         self.columnconfigure(0, weight=1)
 
 
-if __name__ == "___main__":
+if __name__ == "__main__":
     Prog = App(title="Passphrase generator")
     Prog.mainloop()
